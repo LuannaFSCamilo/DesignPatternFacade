fa1ba214b89cee6daf387d6fc41e28d4a13c70d8
LuannaFSCamilo <luannafscamilo@gmail.com>
2023-09-30 13:52:27 -0300

    feat: Adiciona SuperClasse Produto com seus atributos e métodos

---
+++ src/Produto.java
@@ -0,0 +1,21 @@
+public class Produto {
+
+    private String descricao;
+    private String preco;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+}
